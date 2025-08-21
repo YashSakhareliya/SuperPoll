dd87e9ce8ee8627e9dcaf5e68727b07ce43c0609
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-08-21 21:12:06 +0530

    Server Created

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./index');
+
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
