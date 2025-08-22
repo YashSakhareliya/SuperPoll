609801d4b3f55216551ec30f1bf3489bae8cfa68
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-08-22 14:42:33 +0530

    update: poll route Done

--- Server/routes/polls.route.js
+++ Server/routes/polls.route.js
@@ -1,12 +1,15 @@
 import express from "express";
+import { createPoll } from "../controllers/index.js";
 
 const router = express.Router();
 
 // Post req
 // create new poll - return details about poll with qrcode shareId and wpShereId
+router.post('/', createPoll)
 
 // Get req
 // get poll details - user open url so hit this route - return details about poll
+router.get('/:id', getPoll)
 
 // Get req
 // get new qe for poll
