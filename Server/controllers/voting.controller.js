34a170f67132c1e9f49191da92158fe8e12882bf
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-08-23 10:35:00 +0530

    fix: cors issue

--- Server/controllers/voting.controller.js
+++ Server/controllers/voting.controller.js
@@ -111,7 +111,7 @@ const castVote = async (req, res) => {
             res.cookie(`vote_${pollId}`, voteToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
-                sameSite: "lax",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
             })
 
